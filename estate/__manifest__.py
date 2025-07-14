05874b5fef81431a0aa8ae38f38c4ad5c834e3ae
Ahmad Restom <ahmadrestom11@gmail.com>
2025-07-14 13:29:27 +0300

    Moved menus to the last entry in __manifest__.py

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -9,13 +9,13 @@
     'data': [
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
-        'views/estate_menus.xml',
         'views/estate_property_type_views.xml',
         'views/estate_property_tag_views.xml',
         'views/estate_property_offer_views.xml',
         'views/res_users_views.xml',
         'report/estate_property_reports.xml',
-        'report/estate_property_templates.xml'
+        'report/estate_property_templates.xml',
+        'views/estate_menus.xml'
     ],
     'installable': True,
     'application': False,
